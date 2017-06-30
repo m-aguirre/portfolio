59a69d6dd5c9c789b30d5afd80243406b1b3a6ee
m-aguirre <mpaguirre@dons.usfca.edu>
2017-06-29 21:47:23 -0700

    Starter header renders

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Header extends React.Component {
+  constructor(props){
+    super(props)
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Header Renders</h2>
+      </div>
+    )
+  }
+
+}
+
+export default Header;
