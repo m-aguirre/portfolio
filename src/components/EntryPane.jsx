eceeb654e57b75729f7ff2306803f900e0951164
m-aguirre <mpaguirre@dons.usfca.edu>
2017-07-01 14:46:04 -0700

    Entry Pane Background Image

--- src/components/EntryPane.jsx
+++ src/components/EntryPane.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
-//const CSSTransitionGroup = React.addons.CSSTransitionGroup;
-//const TransitionGroup = React.addons.TransitionGroup;
 
 import Pane from './Pane.jsx';
 
@@ -18,7 +16,7 @@ class EntryPane extends React.Component {
 
    render() {
        return <div>
-           <CSSTransitionGroup transitionName="example">
+           <CSSTransitionGroup transitionName="entry-pane">
              { this.state.visible ?  <Pane handleClick={this.handleClick}/> : null }
            </CSSTransitionGroup>
        </div>
