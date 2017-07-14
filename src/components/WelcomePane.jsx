1ec05c9b765eb4dfe5e9fcd404d8a7dde6a48e8f
m-aguirre <mpaguirre@dons.usfca.edu>
2017-07-13 19:03:20 -0700

    fonts added

--- src/components/WelcomePane.jsx
+++ src/components/WelcomePane.jsx
@@ -11,8 +11,14 @@ class WelcomePane extends React.Component {
   render() {
     return (
       <div className="welcome-pane">
-        <h1>Welcome!</h1>
-        <p>Here is a little blurb about me</p>
+        <h1>Engineer - Scientist - Artist</h1>
+        <p>Hello and welcome! I am a full stack software engineer located in
+        San Francisco.  My educational background is full of an assortment of
+        math and science classes and I love taking on tough technical challenges.
+        I enjoy taking a user-centric approach to developing intuitive and good looking
+        applications that satisfy my inner artist.</p>
+      <p>Feel free to contact me with questions, and note that I am currently available
+      for select freelance projects <a href="mailto:matthew.p.aguirre@gmail.com">matthew.p.aguirre@gmail.com</a> </p>
       </div>
     )
   }
--- src/components/projects/Buzz.jsx
+++ src/components/projects/Buzz.jsx
@@ -10,7 +10,20 @@ class Buzz extends React.Component {
   render() {
     return (
       <div className="project-content-pane">
-        <h1>Buzz</h1>
+        <div className="project-content-container">
+          <h1>Buzz</h1>
+          <p>Buzz is an app that allows users to find happy hours in select neighborhoods of San Francisco.
+          Users are given the option to find happy hours in a specific area, or by our fun custom categories that
+          fit whatever mood they're looking to capture - such as divey, bougie, sports bars, etc.</p>
+
+        <p>Buzz was built responsibly with React, Node/Express, and Postgresql</p>
+
+        <div className="project-image" id="buzz-home"></div>
+        <p>Description</p>
+        <div className="project-image" id="buzz-vibes"></div>
+        <p>Description</p>
+        <div className="project-image" id="buzz-happy-hours"></div>
+        </div>
       </div>
     )
   }
