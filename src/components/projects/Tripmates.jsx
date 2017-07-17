56ac2eff8b4470cbd384de3083fb373e634202fe
m-aguirre <mpaguirre@dons.usfca.edu>
2017-07-17 16:53:04 -0700

    index and style changes

--- src/components/projects/Tripmates.jsx
+++ src/components/projects/Tripmates.jsx
@@ -19,11 +19,11 @@ class TripMates extends React.Component {
           <p>Tripmates was built with friends in React, Node/Express, and MySQL.  It includes the
           Yelp API for easily finding activities, and the Facebook API for optional FB authentication.</p>
 
-          <div className="project-image" id="tm-home1"></div>
+        <div className="tripmates-image" id="tm-home1"></div>
           <p>Home page with login/logout, trip creation button, and list of popular destinations</p>
-          <div className="project-image" id="tm-trip1"></div>
+          <div className="tripmates-image" id="tm-trip1"></div>
           <p>Trip planning page</p>
-          <div className="project-image" id="tm-trip-invite"></div>
+          <div className="tripmates-image" id="tm-trip-invite"></div>
           <p>Once a trip's details have been filled in, users can invite their friends to join on their trip</p>
         </div>
       </div>
